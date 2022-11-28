8b37ad3b75fab5c007b89f8550fa2f59b94a835d
DESKTOP-2PA9VA1\DELL <ibrahim15-14295@diu.edu.bd>
2022-11-28 19:49:42 +0600

    Updates

---
+++ precache-manifest.58ef4a5de4c12739bf6901beb0d2129d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d67ee2a7caf63705d514",
+    "url": "git@github.com:Ibrahimkhalil112/Hostel-Management-System-By-React.js.git/static/css/main.fe89daba.chunk.css"
+  },
+  {
+    "revision": "d67ee2a7caf63705d514",
+    "url": "git@github.com:Ibrahimkhalil112/Hostel-Management-System-By-React.js.git/static/js/main.e2f46bf6.chunk.js"
+  },
+  {
+    "revision": "87a9cf5e62cdb8df7a55",
+    "url": "git@github.com:Ibrahimkhalil112/Hostel-Management-System-By-React.js.git/static/js/runtime~main.0ff12fa0.js"
+  },
+  {
+    "revision": "d9957779685bf50a97f1",
+    "url": "git@github.com:Ibrahimkhalil112/Hostel-Management-System-By-React.js.git/static/js/2.958481bc.chunk.js"
+  },
+  {
+    "revision": "7b381c63b71f07fd5d54",
+    "url": "git@github.com:Ibrahimkhalil112/Hostel-Management-System-By-React.js.git/static/js/3.bfb36310.chunk.js"
+  },
+  {
+    "revision": "e94610fb16969f33547ac957726db76a",
+    "url": "git@github.com:Ibrahimkhalil112/Hostel-Management-System-By-React.js.git/index.html"
+  }
+];
